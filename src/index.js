8f0975738a49cdb697603c47fe2dd62ffdf2c7f3
TANMAY JAIN <tanmayjain613@gmail.com>
2024-07-23 23:21:15 +0530

    db setting 1

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", () => {
+            console.log("ERR: " error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening at Port ${process.env.PORT}`)
+        })
+
+        
+    } catch (error) {
+        console.error("ERROR:", error )
+        throw err
+    }
+})()
+    */
